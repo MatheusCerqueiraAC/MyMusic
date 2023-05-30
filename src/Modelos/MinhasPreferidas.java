b7fc5878534dcfc755624a1d982b670c88d1c891
Matheus <matheuscerqueiratb@gmail.com>
2023-05-29 22:28:28 -0300

    Programa de musica

---
+++ src/Modelos/MinhasPreferidas.java
@@ -0,0 +1,13 @@
+package Modelos;
+
+public class MinhasPreferidas {
+
+    public void inclui(Audio audio) {
+        if (audio.getClassificacao() >= 9) {
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto e preferido por todos");
+        } else {
+            System.out.println(audio.getTitulo() + " Tambem é um dos que todo mundo está curtindo");
+        }
+    }
+    
+}
---
+++ src/Principal/Principal.java
@@ -0,0 +1,41 @@
+package Principal;
+
+import Modelos.MinhasPreferidas;
+import Modelos.Musica;
+import Modelos.Podcast;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Bolha Dev");
+        meuPodcast.setApresentador("Marcos Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
+    
+}
